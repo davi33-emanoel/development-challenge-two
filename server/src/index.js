69df20d054214d4def7ccf00cf233db64e2065e9
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-11 20:37:15 -0300

    move req

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const Client = require('./models/client');
 const app = express();
+app.use(express.json());
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
 
 app.listen(port, () =>{
     console.log("app runnig on port 5000");
