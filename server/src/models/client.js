f4fa59208d2ceeaf864d83ac47fab033d55abe5f
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-11 20:37:42 -0300

    fix model client

--- server/src/models/client.js
+++ server/src/models/client.js
@@ -2,24 +2,14 @@ const dynamoose = require('../database/index.js');
 const {v4} = require('uuid');
 
 const client = new dynamoose.Schema({
-    id: {
+    id:{
         type: String,
-        hashKey: true,
-        default: v4(),
+        default: v4()
     },
-    name: {
-        type: String
-    },
-    email:{
-        type: String
-    },
-    birthdate:{
-        type: Date
-    },
-    address:{
-        type: String
-    },
-    timestamps: true
+    name: String,
+    email:String,
+    birthdate:String,
+    address:String
 });
 
 module.exports = dynamoose.model("Client", client);
