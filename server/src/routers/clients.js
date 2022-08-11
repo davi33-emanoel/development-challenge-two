95a8bb706f279d51c84dd07b17f5c1f303a87eee
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-11 20:44:06 -0300

    add clients routes

--- server/src/routers/clients.js
+++ server/src/routers/clients.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Client = require('../models/client');
 const clientRouter = express.Router();
 
 clientRouter.get('/client', (req, res) => {
@@ -8,6 +9,16 @@ clientRouter.get('/client', (req, res) => {
 clientRouter.post('/client', async (req, res) => {
     const client = await Client.create(req.body);
     return res.json(client);
+})
+
+clientRouter.put('/client/:id', async (req, res) => {
+    
+    return ;
+})
+
+clientRouter.delete('/client/:id', async (req, res) => {
+
+    return ;
 })
 
 module.exports = clientRouter;
