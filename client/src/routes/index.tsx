8dd42de3263b2abc6dbd8395aee2ae6b9adedf33
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-11 23:13:24 -0300

    add router dom

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { };
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () =>{
+    return(
+        <Routes>
+            <Route path="/home" element={<p>Meu deus meu senhor me ajude por favor</p>}/>
+            <Route path="*" element={<Navigate to="/home"/>}/>
+        </Routes>
+    );
+}
