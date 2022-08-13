a56103b17d87e3f6d486511fe50abb1525f3e469
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-13 10:37:23 -0300

    add route /home

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 export const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path="/home" element={<p>Meu deus meu senhor me ajude por favor</p>}/>
+            <Route path="/home" element={<Navigate to="/home"/>}/>
             <Route path="*" element={<Navigate to="/home"/>}/>
         </Routes>
     );
