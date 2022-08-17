c9ddec9b9cc0531b5b9285f373b9bc9f1867e682
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-17 00:48:50 -0300

    att sideMenu

--- client/src/shared/components/side-menu/SideMenu.tsx
+++ client/src/shared/components/side-menu/SideMenu.tsx
@@ -6,21 +6,26 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/Menu';
 import {MuiDrawer} from '../drawer/Index'
+import { Outlet } from 'react-router-dom';
+import { Avatar } from '@mui/material';
+import Image from '../../../images/remove_background.png'
 
 export const SideMenu = () => {
   const listStyle3 = {
     background: '#002137',
+    margin:0,
+    height:0
   }
   return (
+    <>
     <Box sx={{ flexGrow: 1}}>
       <AppBar position="static" >
         <Toolbar style={listStyle3}>
             <MuiDrawer/>
-          <Typography variant="h3" textAlign={'left'} component="div" sx={{ flexGrow: 1, textDecoration:'none',color: 'inherit'}}>
-            Medcloud
-          </Typography>
+            <Avatar sx={{height:150, width:150, right:15}} src={Image} component="a" href="/home" title="Home"></Avatar>
         </Toolbar>
       </AppBar>
     </Box>
+    </>
   );
 };
