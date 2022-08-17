a1cc48567aca4585bd175556023c9bafe5166412
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-17 00:50:36 -0300

    att drawer component

--- client/src/shared/components/drawer/Index.tsx
+++ client/src/shared/components/drawer/Index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { Drawer, Box, Typography, IconButton, Avatar, Divider, Button,} from '@mui/material';
+import { Drawer, Box, Typography, IconButton, Avatar, Divider, Button, Switch} from '@mui/material';
 import { useState } from 'react';
 import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
 import EditIcon from '@mui/icons-material/Edit';
 import MenuIcon from '@mui/icons-material/Menu';
+import { BrowserRouter, Route} from 'react-router-dom';
+import { Form } from '../form/Form';
+import Image from '../../../images/remove_background.png'
 
 export const MuiDrawer= () => {
     const [isDrawerOpen, setIsDrawerOpen]= useState(false)
@@ -15,12 +18,12 @@ export const MuiDrawer= () => {
     </IconButton>
         <Drawer anchor='left' open={isDrawerOpen} onClose={()=> setIsDrawerOpen(false)}>
           <Box p={2} width='200px' textAlign='center' role='presentation' bgcolor={'#002137'}>
-          <Avatar sx={{height:120, width:120, display:'flex', left:30}} src="https://medcloud.link/logos/open-graph-logo-large-br.png" component="a" href="" ></Avatar>
+          <Avatar sx={{height:120, width:120, display:'flex', left:30}} src={Image} component="a" href="/home" title="Home"></Avatar>
           </Box>
           <Divider></Divider>
-          <Button variant="text" sx={{marginTop:1}}><MedicalInformationIcon/> Cadastrar novo cliente</Button>
+          <Button variant="text" sx={{marginTop:1}} component="a" href="/add_user"><MedicalInformationIcon/> Cadastrar novo cliente</Button>
           <Divider></Divider>
-          <Button variant="text" sx={{marginTop:1}}><EditIcon></EditIcon> Editar Cadastro</Button>
+          <Button variant="text" sx={{marginTop:1}} component="a" href="/view_users"><EditIcon></EditIcon> Editar Cadastro</Button>
           <Divider></Divider>
       </Drawer>
     </>
