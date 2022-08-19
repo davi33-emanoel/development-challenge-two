ac5fd168f97816c60d53cfce393ef71499124085
davi33-emanoel <emanoeldavi348@gmail.com>
2022-08-18 22:38:18 -0300

    update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,11 @@
-import { AppBar, Typography } from '@mui/material';
 import { BrowserRouter} from 'react-router-dom';
 import { AppRoutes } from './routes';
-import {SideMenu} from './shared/components/index.ts'
-import {Form} from './shared/components/index.ts'
-import {DataTable} from './shared/components/list/List'
-
+import {Home} from './shared/components/index.js'
 
 function App() {
   return (
     <BrowserRouter>
-    <SideMenu>
     <AppRoutes/>
-    </SideMenu>
-    <DataTable></DataTable>
     </BrowserRouter>
   )
 }
